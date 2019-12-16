f6f7580662842739714eafa2013c0e1f11058700
Yurii Oliiarnyk <oliiarnyk14@gmail.com>
2019-12-16 11:00:28 +0200

    Create project screens

---
+++ src/screens/BookedScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const BookedScreen = props => {
+  return (
+    <View style={styles.center}>
+      <Text>BookedScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+
+export default BookedScreen;
